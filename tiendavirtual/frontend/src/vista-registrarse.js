0b35b0dc4c48e88d0b0ff06dc893dc1b1c88bfb8
juansoler <juansoler@gmail.com>
2021-03-04 21:44:25 +0100

    vista registrarse completada

--- tiendavirtual/frontend/src/vista-registrarse.js
+++ tiendavirtual/frontend/src/vista-registrarse.js
@@ -1,24 +1,30 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-text-field/src/vaadin-password-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-registrarse`
- *
- * VistaRegistrarse element.
- *
- * @customElement
- * @polymer
- */
 class VistaRegistrarse extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-self: center; padding: var(--lumo-space-m); flex-direction: column; align-items: center; justify-content: flex-start;" id="vaadinVerticalLayout" theme="spacing">
+ <vaadin-text-field id="nombre" style="align-self: center; width: 100%; margin: var(--lumo-space-m); padding: var(--lumo-space-m);" required label="Nombre"></vaadin-text-field>
+ <vaadin-text-field style="align-self: center; width: 100%; margin: var(--lumo-space-m); padding: var(--lumo-space-m);" required label="Apellidos" id="apellidos"></vaadin-text-field>
+ <vaadin-text-field style="align-self: center; width: 100%; margin: var(--lumo-space-m); padding: var(--lumo-space-m);" required label="e-mail" id="email"></vaadin-text-field>
+ <vaadin-password-field id="password" style="width: 100%; margin: var(--lumo-space-m); padding: var(--lumo-space-m);" required label="password"></vaadin-password-field>
+ <vaadin-password-field id="password2" style="width: 100%; margin: var(--lumo-space-m); padding: var(--lumo-space-m);" label="password2" required></vaadin-password-field>
+ <vaadin-button id="registrarseButton" style="width: 100%;">
+  Registrarse
+ </vaadin-button>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
--- tiendavirtual/src/main/java/vistas/VistaRegistrarse.java
+++ tiendavirtual/src/main/java/vistas/VistaRegistrarse.java
@@ -4,6 +4,11 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.button.Button;
 
 /**
  * A Designer generated component for the vista-registrarse template.
@@ -15,7 +20,21 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 @JsModule("./src/vista-registrarse.js")
 public class VistaRegistrarse extends PolymerTemplate<VistaRegistrarse.VistaRegistrarseModel> {
 
-    /**
+    @Id("nombre")
+	private TextField nombre;
+	@Id("password")
+	private PasswordField password;
+	@Id("email")
+	private TextField email;
+	@Id("apellidos")
+	private TextField apellidos;
+	@Id("password2")
+	private PasswordField password2;
+	@Id("registrarseButton")
+	private Button vaadinButton;
+	@Id("registrarseButton")
+	private Button registrarseButton;
+	/**
      * Creates a new VistaRegistrarse.
      */
     public VistaRegistrarse() {
